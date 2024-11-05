e64c56a3bc27a339e408fb2ab09d9f96bcfe5167
Ahmad07o <aj120262@gmail.com>
2024-11-05 12:46:36 +0000

    variables and data types

---
+++ .devcontainer/Basics/variables.js
@@ -0,0 +1,38 @@
+/*"use strict"; // treat all js code as new version
+const accId=1122
+let accName='Ahmad'
+var accEmail='ahmadButt.com'
+accCity="gujrawala"
+console.log("Before change values")
+console.table([accId,accName,accEmail,accCity])
+console.log("After change values")
+accName="Eman"
+accEmail="emanshahid@gmail.com"
+accCity="Sialkot"
+console.table([accId,accName,accEmail,accCity])*/
+//alert() we are using nodejs not browser console
+//for documentation read EcamaScrpit
+// data types
+//number=> 2 power 53
+//string
+// boolean
+// symbol=>unique
+//bigInt
+//null the data type of null is object
+//undefined
+
+let num="33"
+
+console.log("Before type casting:",typeof(num))
+let val=Number(num)
+console.log("After type casting:",typeof(val),"/Value:",val);
+let ahmad="5year"
+console.log("Type:",typeof ahmad)
+console.log("Type casting");
+let cd=Number(ahmad)
+console.log("type:",typeof cd);
+console.log("print a value:", cd)
+
+
+
+
