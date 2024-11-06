9730f4c20402dc33f044d9d4a904a7e61ddc0c9b
Ahmad07o <aj120262@gmail.com>
2024-11-06 14:35:01 +0000

    strings

--- .devcontainer/Basics/variables.js
+++ .devcontainer/Basics/variables.js
@@ -21,7 +21,7 @@ console.table([accId,accName,accEmail,accCity])*/
 //null the data type of null is object
 //undefined
 
-let num="33"
+/*let num="33"
 
 console.log("Before type casting:",typeof(num))
 let val=Number(num)
@@ -31,7 +31,52 @@ console.log("Type:",typeof ahmad)
 console.log("Type casting");
 let cd=Number(ahmad)
 console.log("type:",typeof cd);
-console.log("print a value:", cd)
+console.log("print a value:", cd)*/
+//let score=undefined
+//console.log("Data type before casting:",typeof (score))
+//console.log("After type casting");
+//let scor=String(score)
+//console.log("Data type:",typeof scor);
+//console.log("value:",scor)
+// type casting
+// "33"=>33
+//"33abc"=>NaN
+//true=>1, false=>0
+//""=>false
+//"ahmad"=>true
+//************operation********
+//console.log("2">1)
+//console.log("h"<=1);
+//console.log("h">1);
+//console.log(true>=1)
+//console.log(false<1);
+//console.log(null>0);
+//console.log(null<0);
+//console.log(null>=0)
+//console.log(null==0)
+//premitive values(stack), non premitive (heap)
+let email1="Emanshaid@gmail.com"
+let email2=email1
+console.log(email1);
+console.log("before changing value:",email2);
+email2="ahmadjaved001@umt"
+console.log("after changing value of email2:",email2);
+name= new String("ahmad")//we can declare string in this way also
+console.log( name.toUpperCase());//convert it into uppercase
+console.log(name);
+const email3="hello there"
+console.log(email3.slice(2))//slice is use to break string and make it new string
+console.log(email2.charAt(3))
+console.log(email2.indexOf('a'))
+const str=String("      just    ")
+console.log(str.trim())//remove extra spaces
+
+
+
+
+
+
+
 
 
 
