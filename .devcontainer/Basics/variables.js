8c4a40550bdf4628139ada8d90eb34479973a930
Ahmad07o <aj120262@gmail.com>
2024-11-10 08:33:56 +0000

    arraysindeep

--- .devcontainer/Basics/variables.js
+++ .devcontainer/Basics/variables.js
@@ -84,6 +84,7 @@ console.log(str.trim())//remove extra spaces
 //console.log(myDate.toString())         
 
 //Arrays
+/*
 const numbers=[1,2,3,4,5]
 console.log(numbers)
 const array=new Array(1,2,3,4,5)// another way to decalre array
@@ -93,16 +94,28 @@ console.log(numbers);
 console.log(numbers.includes(6))
 console.log(numbers.indexOf(6))
 console.log(numbers.pop())
-numbers.unshift(87)
-numbers.shift()
-const myArr=numbers.slice(1,3)
-console.log("A",myArr)
-
-
-
-
-
-
-
+numbers.unshift(87)//  this will add (87) at first elment of the array 
+numbers.shift()// this will remove first element of the array
+//const myArr=numbers.slice(1,3) //this will return an array that contain index 1 to 2 element 
+//console.log("A",myArr)
+//number.splice(1,3) it  will return array that contain 1 to 3 element of index and will remove those elements from original array
+*/
+//const superStars=["sharukh Khan","Suleman khan","amir khan"]
+//const singers=["honey singh","karan ujla","ap dhilon"]
+//superStars.push(singers)//push function will push singer whole array as an element into superStar array
+//console.log(superStars);
+//const allStars=singers.concat(superStars)//concat return be new array so we have to decalre new array to save that array
+//console.log(allStars)
+//const newArray=[...superStars,...singers]//this spread operator and will merge these two arrays
+//console.log(newArray)
+//console.log(Array.isArray("Ahmad"))//in this way we can check weather it is array or not
+//onsole.log(Array.from("Ahmad"))
+const numbers=[1,2,3,[4,5,6],7,8,9]
+const newFlatArray=numbers.flat(Infinity)//this way we can flat our array into single array
+console.log(newFlatArray)
+let score1=100
+let score2=200
+let score3=300
+console.log(Array.of(score1,score2,score3))
 
 
